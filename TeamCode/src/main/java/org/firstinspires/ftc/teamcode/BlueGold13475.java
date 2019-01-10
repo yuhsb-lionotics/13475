5ffb16e6f180e9bccebb3f850daf8d2d69b1fd3b
Zachary Mankowitz <>
2019-01-10 11:15:09 -0500

    hello world take 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueGold13475.java
@@ -0,0 +1,230 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+    @Autonomous(name = "BlueGold13475", group = "Autonomous")
+//@Disabled
+    public class BlueGold13475 extends LinearOpMode {
+        private GoldAlignDetector detector;
+
+        private DcMotor leftDrive = null;
+        private DcMotor rightDrive = null;
+        private DcMotor landerRiser = null;
+
+        private ElapsedTime runtime = new ElapsedTime();
+
+        static final double COUNTS_PER_MOTOR_REV = 1220;    // eg: TETRIX Motor Encoder
+        static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+        static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+        static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                (WHEEL_DIAMETER_INCHES * 3.1415);
+        static final double DRIVE_SPEED = 0.6;
+        static final double TURN_SPEED = 0.5;
+        static final double RISER_SPEED = 0.5;
+
+
+        @Override
+        public void runOpMode() {
+
+            setUp();
+            waitForStart();
+            double goldPos = detector.getXPosition();// this gets gold location - in theory.
+            if(goldPos >=200/*fake #*/){
+                encoderDrive(DRIVE_SPEED, 4.5, -4.5, 0, 5.0);//turn 60
+                encoderDrive(DRIVE_SPEED, 17, 17, 0, 5.0); //move to ball 1
+                encoderDrive(DRIVE_SPEED, 6.5, -6.5, 0, 5.0);//turn 60
+                encoderDrive(DRIVE_SPEED, 10, 10, 0, 5.0);//backup
+                encoderDrive(DRIVE_SPEED, -58, -58, 0, 5.0);//backup
+            }
+            else if((goldPos <=350) && (goldPos >=201)/*fake #*/){
+                encoderDrive(DRIVE_SPEED, 17, 17, 0, 5.0); //move to ball 2
+                encoderDrive(DRIVE_SPEED, 12, -12, 0, 5.0);//turn 90
+                encoderDrive(DRIVE_SPEED, -48, -48, 0, 5.0);//backup
+            }
+            else if((goldPos >=350)){
+                encoderDrive(DRIVE_SPEED, -4.5, 4.5, 0, 5.0);//turn -60
+                encoderDrive(DRIVE_SPEED, 17, 17, 0, 5.0); //move to ball 3
+                encoderDrive(DRIVE_SPEED, -6.5, 6.5, 0, 5.0);//turn 60
+                encoderDrive(DRIVE_SPEED, -28, -28, 0, 5.0);//backup to square
+            }
+
+
+
+            // Step through each leg of the path,
+            // Note: Reverse movement is obtained by setting a negative distance (not speed).
+            //Timeout is how much time is allotted for the motor to get to a desired position
+            //i am assuming 90 turn is 12'
+
+            //_______rightDrive is on backwards
+
+            //this is code to disembark
+            //encoderDrive(RISER_SPEED, 0, 0, 7, 15.0); //riser
+            //encoderDrive(DRIVE_SPEED, 4, 4, 0, 5.0);//move after land
+            //encoderDrive(TURN_SPEED, 8, -8, 0, 5.0); // spin 60
+
+            //code to balls
+//            encoderDrive(DRIVE_SPEED, 4.5, -4.5, 0, 5.0);//turn 60
+//            encoderDrive(DRIVE_SPEED, 17, 17, 0, 5.0); //move to ball 1
+//            encoderDrive(DRIVE_SPEED, 6.5, -6.5, 0, 5.0);//turn 60
+//            encoderDrive(DRIVE_SPEED, 10, 10, 0, 5.0);//backup
+            //check ball
+
+            encoderDrive(DRIVE_SPEED, -10, -10, 0, 5.0);//move to ball two
+            //check
+            encoderDrive(DRIVE_SPEED, -10.25, -10.25, 0, 5.0);//move to ball three
+            //check
+
+            //code to sqver
+            //encoderDrive(DRIVE_SPEED, -28, -28, 0, 5.0);//move to turn position
+            encoderDrive(DRIVE_SPEED, -4.2, 4.2, 0, 5.0);
+            encoderDrive(DRIVE_SPEED, 57, 57, 0, 5.0);//move to square
+
+            //to carter
+            encoderDrive(DRIVE_SPEED, 12.25, -12.25, 0, 5.0);//180 turn
+            encoderDrive(DRIVE_SPEED, 75, 75, 0, 5.0);
+
+
+            telemetry.addData("Path", "Complete");
+            telemetry.update();
+        }
+
+        /*
+         *  Method to perfmorm a relative move, based on encoder counts.
+         *  Encoders are not reset as the move is based on the current position.
+         *  Move will stop if any of three conditions occur:
+         *  1) Move gets to the desired position
+         *  2) Move runs out of time
+         *  3) Driver stops the opmode running.
+         */
+        public void encoderDrive(double speed,
+                                 double leftInches, double rightInches, double riser,
+                                 double timeoutS) {
+            int newLeftTarget;
+            int newRightTarget;
+            int newRiserTarget;
+
+            // Ensure that the opmode is still active
+            if (opModeIsActive()) {
+
+                // Determine new target position, and pass to motor controller
+                newLeftTarget = leftDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+                newRightTarget = rightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+
+                newRiserTarget = landerRiser.getCurrentPosition() + (int) (riser * COUNTS_PER_INCH);
+
+                leftDrive.setTargetPosition(newLeftTarget);
+                rightDrive.setTargetPosition(newRightTarget);
+
+                landerRiser.setTargetPosition(newRiserTarget);
+
+                // Turn On RUN_TO_POSITION
+                leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                landerRiser.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                // reset the timeout time and start motion.
+                runtime.reset();
+                leftDrive.setPower(Math.abs(speed));
+                rightDrive.setPower(Math.abs(speed));
+                landerRiser.setPower(Math.abs(speed));
+
+                // keep looping while we are still active, and there is time left, and both motors are running.
+                // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+                // its target position, the motion will stop.  This is "safer" in the event that the robot will
+                // always end the motion as soon as possible.
+                // However, if you require that BOTH motors have finished their moves before the robot continues
+                // onto the next step, use (isBusy() || isBusy()) in the loop test.
+                while (opModeIsActive() &&
+                        (runtime.seconds() < timeoutS) &&
+                        (leftDrive.isBusy() && rightDrive.isBusy() || landerRiser.isBusy())) {
+
+                    // Display it for the driver.
+                    telemetry.addData("Path1", "Running to %7d :%7d :%7d", newLeftTarget, newRightTarget, newRiserTarget);
+                    telemetry.addData("Path2", "Running at %7d :%7d :%7d",
+                            leftDrive.getCurrentPosition(),
+                            rightDrive.getCurrentPosition(),
+                            landerRiser.getCurrentPosition());
+                    telemetry.update();
+                }
+
+                // Stop all motion;
+                leftDrive.setPower(0);
+                rightDrive.setPower(0);
+                landerRiser.setPower(0);
+
+                // Turn off RUN_TO_POSITION
+                leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                landerRiser.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                //  sleep(250);   // optional pause after each move
+            }
+        }
+
+        private void setUp() {
+     /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+            leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+            rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+            landerRiser = hardwareMap.get(DcMotor.class, "lander_riser");
+
+            leftDrive.setDirection(DcMotor.Direction.FORWARD);
+            rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+
+            // Send telemetry message to signify robot waiting;
+            telemetry.addData("Status", "Resetting Encoders");
+            telemetry.update();
+
+            leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            landerRiser.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            landerRiser.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            // Send telemetry message to indicate successful Encoder reset
+            telemetry.addData("Path0", "Starting at %7d :%7d :%7d",
+                    leftDrive.getCurrentPosition(),
+                    rightDrive.getCurrentPosition(),
+                    landerRiser.getCurrentPosition());
+            telemetry.update();
+
+            //DodeCV stuff
+
+           
+
+            // Set up detector
+            detector = new GoldAlignDetector(); // Create detector
+            detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+            detector.useDefaults(); // Set detector to use default settings
+
+            // Optional tuning
+            detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+            detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+            detector.downscale = 0.4; // How much to downscale the input frames
+
+            detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+            //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+            detector.maxAreaScorer.weight = 0.005; //
+
+            detector.ratioScorer.weight = 5; //
+            detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+            detector.enable(); // Start the detector!
+
+        }
+
+    }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled13475.java
@@ -0,0 +1,148 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="DriverControlled13475", group="LinearOpMode")
+//@Disabled
+public class DriverControlled13475 extends LinearOpMode {
+
+    // Declare OpMode members.
+    //private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor landerRiser = null;
+
+    private DcMotor spinnyArmExt = null;
+    private DcMotor spinnyArmTilt1 = null;
+    private DcMotor spinnyArmTilt2 = null;
+
+
+    //following is tbt:
+    private CRServo spinner1 = null;
+    private CRServo spinner2 = null;
+
+
+    @Override
+    public void runOpMode() {
+        setUp();
+        boolean flipperCount=false;
+
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //motors
+
+            float driveLeft = gamepad1.left_stick_y;
+            float driveRight  =  gamepad1.right_stick_y;
+            float landerRiserpwr = gamepad1.left_trigger-gamepad1.right_trigger;
+
+            float spinnyArmExtpwr=gamepad2.right_trigger-gamepad2.left_trigger;
+
+            while(gamepad2.dpad_down){
+                spinnyArmTilt1.setPower(-.3);
+                spinnyArmTilt2.setPower(-.3);
+            }
+            while (gamepad2.dpad_up){
+                spinnyArmTilt1.setPower(.3);
+                spinnyArmTilt2.setPower(.3);
+            }
+            if(!(gamepad2.dpad_up || gamepad2.dpad_down))
+            {
+                spinnyArmTilt1.setPower(0);
+                spinnyArmTilt2.setPower(0);
+            }
+
+
+            leftDrive.setPower(driveLeft);
+            rightDrive.setPower(driveRight);
+            landerRiser.setPower(landerRiserpwr);
+
+            spinnyArmExt.setPower(spinnyArmExtpwr);
+
+
+           //servos code
+
+
+            //spinner (i realized this isn't what tager wants but ill fix later)
+            if(gamepad2.right_bumper){//spin in
+
+                    spinner2.setPower(0);
+                    spinner1.setPower(1);
+
+            }
+            if(gamepad2.left_bumper)
+            {
+                spinner2.setPower(1);
+                spinner1.setPower(0);
+            }
+
+
+
+
+
+
+
+
+
+
+            //feedback();
+        }
+    }
+    private void setUp(){
+        //telemetry.addData("Status", "Initializing. Or is it?");
+        //telemetry.update();
+
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        landerRiser = hardwareMap.get(DcMotor.class, "lander_riser");
+
+        spinnyArmExt = hardwareMap.get(DcMotor.class, "spinny_arm_ext");
+        spinnyArmTilt1 = hardwareMap.get(DcMotor.class, "spinny_arm_tilt1");
+        spinnyArmTilt2 = hardwareMap.get(DcMotor.class, "spinny_arm_tilt2");
+
+
+        spinner1 = hardwareMap.get(CRServo.class, "spinner1");
+        spinner2 = hardwareMap.get(CRServo.class, "spinner2");
+
+
+
+
+        spinnyArmExt.setDirection(DcMotor.Direction.REVERSE);
+        spinnyArmTilt1.setDirection(DcMotor.Direction.FORWARD);
+        spinnyArmTilt2.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        //runtime.reset();
+    }
+    private void feedback(){            // Show the elapsed game time and wheel power.
+        //telemetry.addData("Status", "Run Time: " + runtime.toString());
+        //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftDrive, rightDrive);
+        //telemetry.update();
+    }
+}
