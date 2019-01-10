d2b1527daa3de62e6e4b9f5b13ec3adfcbbb2b1f
Mangowatz <36213811+Mangowatz@users.noreply.github.com>
2019-01-10 11:34:06 -0500

    Update DriverControlled13475.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled13475.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled13475.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-//hello, i am test. dont remove me pls
+//hello, i am test. dont remove me pls - ok, now u can
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
